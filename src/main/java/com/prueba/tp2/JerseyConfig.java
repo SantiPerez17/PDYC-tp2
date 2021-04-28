20c926a12f8c3107e81351cea72d69e6128b3129
SantiPerez17 <santi__@live.com.ar>
2021-04-27 21:33:13 -0300

    edit properties, add jerseyconfig
    ste men, se hacia el  inglezasdansdaklsdas

---
+++ src/main/java/com/prueba/tp2/JerseyConfig.java
@@ -0,0 +1,8 @@
+package com.prueba.tp2;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+@Component
+public class JerseyConfig extends ResourceConfig{
+    
+}
---
+++ src/main/java/com/prueba/tp2/asad.java
@@ -0,0 +1,12 @@
+package com.prueba.tp2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class asad {
+    @GetMapping({"/"})
+	public String index() {
+		return "home";
+	}
+    
+}
